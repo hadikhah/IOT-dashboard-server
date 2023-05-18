e61e8eab787c7021601f04f9efbce5461beaa46a
hadikhah <ma.hadikhah@raahin.com>
2023-05-19 02:06:08 +0330

    add vue inertia dashboard

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,7 +13,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory(10)->create();
+        User::factory()->create([
+            "email" => "demo@demo.demo",
+            "password" => bcrypt("demo123456")
+        ]);
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
