f6ee3457833a609695d146de76f104fb01abcaa6
hadikhah <hadikhahe@gmail.com>
2023-05-21 00:35:23 +0330

    add device vue views

---
+++ resources/js/Pages/Device/Create.vue
@@ -0,0 +1,17 @@
+<template>
+        <Head title="add new device" />
+
+        <div>
+            new device page
+        </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
